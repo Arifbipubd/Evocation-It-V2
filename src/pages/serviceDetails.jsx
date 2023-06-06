f240b04e5b4753fe5dd8a67dbe1f8e9b6f4ecbbd
Arifbipubd <arifulislambipubd@gmail.com>
2023-06-06 13:00:42 +0600

    deploy conflict solve

--- src/pages/serviceDetails.jsx
+++ src/pages/serviceDetails.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import FooterOne from "../common/footer/FooterOne";
 import HeaderOne from "../common/header/HeaderOne";
 import BcrumbBannerOne from "../elements/breadcrumb/BcrumbBannerOne";
@@ -9,19 +9,19 @@ import CtaLayoutOne from "../component/cta/CtaLayoutOne";
 import SectionTitle from "../elements/section-title/SectionTitle";
 import ProjectPropOne from "../component/project/itemProp/ProjectPropOne";
 // import { slugify } from "../utils";
-import ServiceData from "../data/service/ServiceMain.json";
+// import ServiceData from "../data/service/ServiceMain.json";
 import ProjectData from "../data/project/ProjectData.json";
 import ProcessOne from "../component/process/ProcessOne";
 import AboutTwo from "../component/about/AboutTwo";
 import ColorSwitcher from "../elements/switcher/ColorSwitcher";
 import SEO from "../common/SEO";
 
-const allServiceData = ServiceData;
+// const allServiceData = ServiceData;
 const getProjectData = ProjectData;
 
 const ServiceDetails = () => {
-  const params = useParams();
-  const serviceSlug = params.slug;
+  // const params = useParams();
+  // const serviceSlug = params.slug;
 
   // const getServiceData = allServiceData.filter(
   //   (data) => slugify(data.title) === serviceSlug
