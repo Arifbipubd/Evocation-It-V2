8e7eb8096856ed60f65e3452aae9b21730e7b649
Arifbipubd <arifulislambipubd@gmail.com>
2023-08-19 23:02:30 +0600

    title and meta description add

--- src/common/SEO.js
+++ src/common/SEO.js
@@ -4,14 +4,17 @@ import PropTypes from "prop-types";
 
 const SEO = ( {title} ) => {
     return (
-        <>
-            <meta charSet="utf-8" />
-            <title>EvocationIt | {title}</title>
-            <meta name="robots" content="noindex, follow" />
-            <meta name="description" content="Creative Agency, Corporate and Portfolio React JS Template" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        </>
-    )
+      <>
+        <meta charSet='utf-8' />
+        <title>EvocationIt | {title}</title>
+        <meta name='robots' content='noindex, follow' />
+        <meta name='description' content='Create your Digital Landscape.' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+      </>
+    );
 }
 SEO.propTypes = {
     title: PropTypes.string
